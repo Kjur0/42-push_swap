16d863e27e858093f95396f968d9eb3fadf54fd8
kjurkows <kjurkows@student.42warsaw.pl>
2026-08-06 19:59:44 +0200

    Create files and headers for Stack ops
    TODO: add docs
    Signed-off-by: kjurkows <kjurkows@student.42warsaw.pl>

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: kjurkows <kjurkows@student.42warsaw.pl>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/28 15:18:28 by kjurkows          #+#    #+#              #
-#    Updated: 2026/08/06 19:39:51 by kjurkows         ###   ########.fr        #
+#    Updated: 2026/08/06 19:59:22 by kjurkows         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC			=	cc
-CFLAGS		=	-Wall -Wextra -Werror
+CFLAGS		=	-Wall -Wextra -Werror -Iincludes
 
 NAME		=	push_swap
 
@@ -24,7 +24,17 @@ LIBFT_MAKE	=	$(LIBFT_DIR)/Makefile
 LIBFT_FLAGS	=	-I$(LIBFT_DIR)
 LIBFT_LINK	=	-L$(LIBFT_DIR) -lft
 
-SRCS		=
+SRCS		=	sa.c \
+				sb.c \
+				ss.c \
+				pa.c \
+				pb.c \
+				ra.c \
+				rb.c \
+				rr.c \
+				rra.c \
+				rrb.c \
+				rrr.c
 OBJS		=	$(SRCS:%.c=$(OBJS_DIR)/%.o)
 
 all:	$(NAME):
@@ -54,4 +64,6 @@ fclean:				clean
 
 re:					fclean all
 
-.PHONY: all clean fclean re
+libft:				$(LIBFT)
+
+.PHONY: all clean fclean re libft
---
+++ includes/stacks.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stacks.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjurkows <kjurkows@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/06 19:49:16 by kjurkows          #+#    #+#             */
+/*   Updated: 2026/08/06 19:59:05 by kjurkows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACKS_H
+# define STACKS_H
+
+# include <libft.h>
+
+//TODO: DOCS
+
+void	sa(t_list *a);
+void	sb(t_list *b);
+void	ss(t_list *a, t_list *b);
+void	pa(t_list *a, t_list *b);
+void	pb(t_list *a, t_list *b);
+void	ra(t_list *a);
+void	rb(t_list *b);
+void	rr(t_list *a, t_list *b);
+void	rra(t_list *a);
+void	rrb(t_list *b);
+void	rrr(t_list *a, t_list *b);
+
+#endif
